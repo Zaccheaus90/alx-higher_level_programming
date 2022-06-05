2da980a0c48629d6f755f1217753dd3b1d29913a
root <afolabizacchaeus01@gmail.com>
2022-06-04 18:44:44 -0700

    updating

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 
 if __name__ == "__main__":
     """Print the addition of all arguments."""
@@ -8,3 +8,4 @@ if __name__ == "__main__":
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
     print("{}".format(total))
+
